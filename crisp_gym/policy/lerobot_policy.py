f2b14c9e6103253e205bcd243fb276bfd709d1ac
Ben Sturgis <ben.sturgis@tum.de>
2026-08-10 20:30:16 +0200

    fix: compensate torque sensor

--- crisp_gym/policy/lerobot_policy.py
+++ crisp_gym/policy/lerobot_policy.py
@@ -106,12 +106,20 @@ class LerobotPolicy(Policy):
             obs_raw: Observation = self.env.get_obs()
 
             obs_raw["observation.state"] = concatenate_state_features(obs_raw)
+            obs_raw["observation.state"][2] -= 0.021     # DEBUG: compensate for force-torque sensor
 
             if self.task is not None:
                 obs_raw["task"] = self.task
 
             self.parent_conn.send(obs_raw)
             action: Action = self.parent_conn.recv().squeeze(0).to("cpu").numpy()
+            action[3:5] *= 0.1  # DEBUG: no orientation command
+            # if obs_raw["observation.state"][2] <= 0.095:     # DEBUG: avoid driving into the table
+            if obs_raw["observation.state"][2] <= 0.085:        # For task 2
+                action[2] = max(action[2], 0.0)
+            # if action[6] <= 0.2 or (obs_raw["observation.state"][6] >= 0.6 and action[6] <= 0.4):              # Tasks 0 and 1
+            # if action[6] <= 0.2:
+            #     action[6] = 0.01
             logger.debug(f"Action: {action}")
 
             try:
